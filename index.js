255231287010dbbdd202827e333af6a2db8a6009
Thaís Silva <thaiscaroline.cms@gmail.com>
2022-01-11 14:56:27 -0300

    form, partials and database

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser"); 
+const connection = require("./database/database");
 
-//View Engine
+//Database
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com o banco de dados")
+    })
+    .catch((msgErro) => {
+        console.log(msgErro);
+    });
+
+//View engine
 app.set('view engine', 'ejs');
 //Arquivos estáticos
 app.use(express.static('public'));
+//Body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     res.render("index");
@@ -12,6 +27,10 @@ app.get("/", (req, res) => {
 
 app.get("/perguntar", (req, res) => {
     res.render("perguntar");
+});
+
+app.post("/salvarpergunta", (req, res) => {
+    res.send("Formulário recebido!");
 });
 
 app.listen(8080, () => {
